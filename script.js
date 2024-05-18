5bd2630bf580adde12424093d9b39f3c5d565faa
Alexander Xu <152698786+alxu31@users.noreply.github.com>
2024-05-18 10:38:26 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function updateDateTime() {
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+}
+setInterval(updateDateTime, 1000);
+
+function randomImage(){
+  var images = [
+   'backgrounds/b1.jpg',
+   'backgrounds/b2.jpg',
+   'backgrounds/b3.jpg',
+   'backgrounds/b4.jpg',
+   'backgrounds/b5.jpg',
+   'backgrounds/b6.jpg'];
+  var size = images.length;
+  var x = Math.floor(size * Math.random());
+  document.body.style.backgroundImage = "url('"+ images[x] + "')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundAttachment = "fixed";
+}
+document.addEventListener("DOMContentLoaded", randomImage);
+
+
