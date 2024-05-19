0fb510c28b3b2022b31cceeb945cb8f8a9e61ad0
Alexander Xu <152698786+alxu31@users.noreply.github.com>
2024-05-19 18:46:49 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,27 @@ function randomImage(){
    'backgrounds/b3.jpg',
    'backgrounds/b4.jpg',
    'backgrounds/b5.jpg',
-   'backgrounds/b6.jpg'];
+   'backgrounds/b6.jpg',
+   'backgrounds/b7.jpg',
+   'backgrounds/b8.jpg',
+   'backgrounds/b9.jpg',
+   'backgrounds/b10.jpg',
+   'backgrounds/b11.jpg',
+   'backgrounds/b12.jpg',
+   'backgrounds/b13.jpg',
+   'backgrounds/b15.jpg',
+   'backgrounds/b15.jpg',
+   'backgrounds/b16.jpg',
+   'backgrounds/b17.jpg',
+   'backgrounds/b18.jpg',
+   'backgrounds/b19.jpg',
+   'backgrounds/b20.jpg',
+   'backgrounds/b21.jpg',
+   'backgrounds/b22.jpg',
+   'backgrounds/b23.jpg',
+   'backgrounds/b24.jpg',
+   'backgrounds/b25.jpg',
+   'backgrounds/b26.jpg'];
   var size = images.length;
   var x = Math.floor(size * Math.random());
   document.body.style.backgroundImage = "url('"+ images[x] + "')";
